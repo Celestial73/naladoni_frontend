3c1bb6b9a4ed494aede04e756fb0d16bdf176288
Celestial73 <mikhail.ambarnov@mail.ru>
2025-12-14 18:33:39 +0300

    Update Page.tsx

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { backButton } from '@tma.js/sdk-react';
 import { type PropsWithChildren, useEffect } from 'react';
 
-export function Page({ children, back = true }: PropsWithChildren<{
+export function Page({ children, back = false }: PropsWithChildren<{
   /**
    * True if it is allowed to go back from this page.
    */
