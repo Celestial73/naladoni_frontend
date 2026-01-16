62d08a9a97603e358b8e2614bdaee453b47ea410
Celestial73 <mikhail.ambarnov@mail.ru>
2026-01-17 00:17:22 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/naladoni_frontend/',
+  base: '/naladoni/',
   css: {
     preprocessorOptions: {
       scss: {
